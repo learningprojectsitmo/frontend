38e63b2965f5ef83bcdf0bfbbcbfe138297679c2
Anton Afanasiev <anton0afanasiev0v@gmail.com>
2025-11-29 16:18:22 +0300

    add navigation

--- src/components/mainPage_components/BasePage.tsx
+++ src/components/mainPage_components/BasePage.tsx
@@ -1,14 +1,21 @@
 import './BasePage.css';
 import { Sider } from './Sider';
 import { Header } from './Header'
+import type { ReactNode } from 'react';
 
-export function BasePage() {
+interface BasePageProps {
+  children?: ReactNode;
+}
+
+export function BasePage({ children }: BasePageProps) {
   return (
     <div className="outer-wrapper">
       <Sider />
       <div className="header-wrapper">
         <Header />
-        <div></div>
+        <div className="main-content">
+          {children}
+        </div>
       </div>
     </div>
   );
