4a2fb03f58157c5a7f9f83d199fb79acbdb65917
Mansur <ristale86@gmail.com>
2025-12-07 17:31:40 +0300

    createUser button

--- src/pages/createUserPage/CreateUserPage.tsx
+++ src/pages/createUserPage/CreateUserPage.tsx
@@ -1,20 +1,23 @@
-import './ApplicationsPage.css';
+import './createUserPage.css';
 import '../pages.css'
 //import { useContext } from 'react';
 //import { Context } from '../../main';
 import { Sider } from '../../components/mainPage_components/Sider';
 import { Header } from '../../components/mainPage_components/Header'
+import CreateUserForm from '../../components/auth_components/CreateUserForm'
 
-export function ApplicationsPage() {
+export function CreateUserPage() {
 
   //const {store} =useContext(Context);
 
   return (
     <div className="outer-wrapper">
-      <Sider navbarOption={5} />
+      <Sider navbarOption={2} />
       <div className="header-wrapper">
-        <Header pageName="Заявки" profileName="Иванов Иван"/>
-        <div></div>
+        <Header pageName="Создать пользователя" profileName="Иванов Иван"/>
+        <div className="create-user-form-wrapper">
+          <CreateUserForm />
+        </div>
       </div>
     </div>
   );
--- src/pages/personasPage/PersonasPage.tsx
+++ src/pages/personasPage/PersonasPage.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { Context } from '../../main';
 import { Sider } from '../../components/mainPage_components/Sider';
 import { Header } from '../../components/mainPage_components/Header'
+import { Link } from 'react-router';
 
 export function PersonasPage() {
 
@@ -14,7 +15,14 @@ export function PersonasPage() {
       <Sider navbarOption={2}/>
       <div className="header-wrapper">
         <Header pageName="Контакты" profileName="Иванов Иван"/>
-        <div></div>
+        <div className="content-wrapper">
+          <div className="top-bar">
+            <Link to="create" className="link-underline">
+              <button className="create-user-button">Создать пользователя</button>
+            </Link>
+          </div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
