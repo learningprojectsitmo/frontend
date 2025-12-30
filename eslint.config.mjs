da2a2bcff381ae3c5edd59e16955972d01fba0cd
fedor <lesssa886@gmail.com>
2025-12-30 14:29:56 +0300

    updating login form and registration form

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,56 +9,50 @@ import reactDom from "eslint-plugin-react-dom";
 import prettier from "eslint-config-prettier";
 
 export default [
-  // Игнор
-  {
-    ignores: ["dist", "node_modules"],
-  },
-
-  // База
-  js.configs.recommended,
-
-  // TypeScript (БЕЗ type-aware)
-  ...tseslint.configs.recommended,
-
-  // React + TS
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: globals.browser,
+    // Игнор
+    {
+        ignores: ["dist", "node_modules"],
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-x": reactX,
-      "react-dom": reactDom,
+
+    // База
+    js.configs.recommended,
+
+    // TypeScript (БЕЗ type-aware)
+    ...tseslint.configs.recommended,
+
+    // React + TS
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+            "react-x": reactX,
+            "react-dom": reactDom,
+        },
+        rules: {
+            // Hooks
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+
+            // Vite HMR
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+            // React 19
+            "react/react-in-jsx-scope": "off",
+
+            // TS
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/no-explicit-any": "warn",
+
+            // Прод
+            "no-console": "warn",
+        },
     },
-    rules: {
-      // Hooks
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
 
-      // Vite HMR
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-
-      // React 19
-      "react/react-in-jsx-scope": "off",
-
-      // TS
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/no-explicit-any": "warn",
-
-      // Прод
-      "no-console": "warn",
-    },
-  },
-
-  prettier
+    prettier,
 ];
--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,33 +1,41 @@
-import $api from '../../../api/interceptors';
-import qs from 'qs';
-import type { AxiosResponse } from 'axios'
-import type { AuthResponce, RegistrationResponce } from "../../../shared/types/responce/AuthResponce";
+import $api from "../../../api/interceptors";
+import type { AxiosResponse } from "axios";
+import type {
+    AuthResponce,
+    RegistrationResponce,
+} from "../../../shared/types/responce/AuthResponce";
+import type { LoginFormType } from "../../../shared/types/forms/LoginFormType";
+import type { RegistrationBodyForRequest } from "../../../shared/types/forms/RegistrationFormType";
 
 export default class AuthService {
-    static async login(email: string, password: string): Promise<AxiosResponse<AuthResponce>> {
-        return $api.post('/auth/token', qs.stringify({"username": email, "password": password }), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
+    static async login(data: LoginFormType): Promise<AxiosResponse<AuthResponce>> {
+        return $api.post("/auth/token", data, {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        });
     }
 
-    static async registration(firstName: string, middleName: string, lastName: string | undefined, email: string, password: string, isuNumber?: number): Promise<AxiosResponse<RegistrationResponce>> {
+    static async registration(
+        data: RegistrationBodyForRequest,
+    ): Promise<AxiosResponse<RegistrationResponce>> {
         const payload: any = {
-            "email": email,
-            "first_name": firstName,
-            "middle_name": middleName,
-            "password_string": password
-        }
-        
-        if (lastName) {
-            payload.last_name = lastName;
-        }
-        
-        if (isuNumber) {
-            payload.isu_number = isuNumber;
+            email: data.email,
+            first_name: data.firstName,
+            middle_name: data.middleName,
+            password_string: data.password,
+        };
+
+        if (data.lastName !== undefined) {
+            payload.last_name = data.lastName;
         }
 
-        return $api.post('/v1/users', payload, {headers: {'Content-Type': 'application/json'}})
+        if (data.isuNumber !== undefined) {
+            payload.isu_number = data.isuNumber;
+        }
+
+        return $api.post("/v1/users", payload, { headers: { "Content-Type": "application/json" } });
     }
 
     static async logout(): Promise<void> {
-        return $api.post('/v1/auth/logout')
+        return $api.post("/v1/auth/logout");
     }
 }
