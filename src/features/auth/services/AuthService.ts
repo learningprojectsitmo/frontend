b01863534efa54ca3ab7dbc1cfc3f7fb7169dcce
fedor <lesssa886@gmail.com>
2026-01-22 22:44:15 +0300

    fix conflicts

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,41 +1,33 @@
-import $api from "../../../api/interceptors";
-import type { AxiosResponse } from "axios";
-import type {
-    AuthResponce,
-    RegistrationResponce,
-} from "../../../shared/types/responce/AuthResponce";
-import type { LoginFormType } from "../../../shared/types/forms/LoginFormType";
-import type { RegistrationBody, RegistrationBodyForRequest } from "../../../shared/types/forms/RegistrationFormType";
+import $api from '../../../api/interceptors';
+import qs from 'qs';
+import type { AxiosResponse } from 'axios'
+import type { AuthResponce, RegistrationResponce } from "../../../shared/types/responce/AuthResponce";
 
 export default class AuthService {
-    static async login(data: LoginFormType): Promise<AxiosResponse<AuthResponce>> {
-        return $api.post("/auth/token", data, {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        });
+    static async login(email: string, password: string): Promise<AxiosResponse<AuthResponce>> {
+        return $api.post('v1/auth/token', qs.stringify({"grant_type": "password", "username": email, "password": password }), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
     }
 
-    static async registration(
-        data: RegistrationBodyForRequest,
-    ): Promise<AxiosResponse<RegistrationResponce>> {
-        const payload: RegistrationBody = {
-            email: data.email,
-            first_name: data.firstName,
-            middle_name: data.middleName,
-            password: data.password,
-        };
-
-        if (data.lastName !== undefined) {
-            payload.last_name = data.lastName;
+    static async registration(firstName: string, middleName: string, lastName: string | undefined, email: string, password: string, isuNumber?: number): Promise<AxiosResponse<RegistrationResponce>> {
+        const payload: any = {
+            "email": email,
+            "first_name": firstName,
+            "middle_name": middleName,
+            "password_string": password
+        }
+        
+        if (lastName) {
+            payload.last_name = lastName;
+        }
+        
+        if (isuNumber) {
+            payload.isu_number = isuNumber;
         }
 
-        if (data.isuNumber !== undefined) {
-            payload.isu_number = data.isuNumber;
-        }
-
-        return $api.post("/v1/users", payload, { headers: { "Content-Type": "application/json" } });
+        return $api.post('/v1/users', payload, {headers: {'Content-Type': 'application/json'}})
     }
 
     static async logout(): Promise<void> {
-        return $api.post("/v1/auth/logout");
+        return $api.post('/v1/auth/logout')
     }
 }
